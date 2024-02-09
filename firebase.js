961767189bd9176a435313453ac0a9a2f2c8aa8f
david <kaneshiuchiha@gmail.com>
2024-02-08 22:48:13 -0500

    api key fixed

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 
 const firebaseConfig = {
@@ -13,5 +14,7 @@ const firebaseConfig = {
   };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
+export const provider = new GoogleAuthProvider()
 
 export default app
